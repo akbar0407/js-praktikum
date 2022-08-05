9806483c12e25e7bfe69365478bd4870720b03ef
Akbar Novruzov <akbarnzade@gmail.com>
2022-08-05 17:36:19 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+// Task 1
+//const number = parseInt(prompt('Enter number'))
+//const result = parseInt(number % 100 / 10)
+//console.log('Task 1:', result)
+
+// Task2
+//const number = parseInt(prompt('Enter number:'))
+
+//let num = number
+//let length = 0
+//while (num > 0) {
+  //  num = parseInt(num / 10)
+   // length++
+//}
+
+//const n = Math.ceil(length / 2)
+//const result = parseInt((number % (10 ** n)) / (10 ** (n-1)))
+//console.log('Task2:', result)
+
+
+//Task3
+//const number = parseInt(prompt('Enter number:'))
+//const result = parseInt(number / 10)
+//console.log('Task3:', result)
+
+//Task4
+//const number = parseInt(prompt('Enter number:'))
+
+//let num = number
+//let length = 0
+//while (num > 0) {
+  //  num = parseInt(num / 10)
+  // length++
+//}
+//const result = parseInt(number % (10 ** (length - 1)))
+//console.log('Task4:', result)
+
+//Task5
+//const number = parseInt(prompt('Enter number:'))
+
+//if(!(number % 3) && !(number % 5)) {
+//    console.log('Task5:', true)
+//}
+//else {
+//    console.log('Task5:', false)
+//}
+
+//Task6
+//let number = parseInt(prompt('Enter number'))
+
+//let result = false;
+//while (number > 0) {
+//   if (number % 10 === 6) {
+//    result = true
+//    break
+//}
+//number = parseInt(number / 10)
+//}
+//console.log('Task6:', result)
+
+//Task7
+//let number = parseInt(prompt('Enter number'))
+//let num = number + 1;
+//while (n % 11 !== 0) {
+//    num++
+//}
+//alert(num)
+
+//Task8
+
